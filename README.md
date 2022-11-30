fb56f9bf18fea9aface44c2825437e1da14e3879
Azure Zanculmarktum <zanculmarktum@gmail.com>
2022-11-30 20:05:20 +0700

    show popup window

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # cookies.txt-importer-for-chrome
-![Logo](/screenshots/logo.png)
+![Logo](screenshots/logo.png)
 
 ## Description
+Fork of [zhad3's](https://github.com/zhad3/cookies.txt-importer-for-chrome) but instead of opening Options page it shows up a popup window, like so.
+
+![Popup window](screenshots/popup.png)
+
 This extension lets you import cookies through one or more cookies.txt files which are saved in the netscape format.
 See [The Unofficial Cookie FAQ](http://www.cookiecentral.com/faq/#3.5) for more information about the format.
 
 ## Screenshots
-![Screenshot01](/screenshots/preview01.png)![Screenshot02](/screenshots/preview02.png)
+![Screenshot01](screenshots/preview01.png)![Screenshot02](screenshots/preview02.png)
 
 ## Notes
 * Even though the screenshots are in german, the extension is available in english.
--- background.js
+++ background.js
@@ -1,15 +1,16 @@
-chrome.browserAction.onClicked.addListener(function() {
-    chrome.tabs.query({'currentWindow':true, 'active':true}, function(tab) {
-        chrome.storage.local.set({'referer_tab':tab[0]}, function() {
-            if (chrome.runtime.openOptionsPage) {
-                chrome.runtime.openOptionsPage(function(err) {
-                    if (chrome.runtime.lastError != undefined) {
-                        window.open(chrome.runtime.getURL('options.html'));
-                    }
-                });
-            } else {
-                window.open(chrome.runtime.getURL('options.html'));
-            }
-        });
-    });
-});
+//chrome.browserAction.onClicked.addListener(function() {
+//    //chrome.tabs.query({'currentWindow':true, 'active':true}, function(tab) {
+//    //    chrome.storage.local.set({'referer_tab':tab[0]}, function() {
+//    //        if (chrome.runtime.openOptionsPage) {
+//    //            chrome.runtime.openOptionsPage(function(err) {
+//    //                if (chrome.runtime.lastError != undefined) {
+//    //                    window.open(chrome.runtime.getURL('options.html'));
+//    //                }
+//    //            });
+//    //        } else {
+//    //            window.open(chrome.runtime.getURL('options.html'));
+//    //        }
+//    //    });
+//    //});
+//    window.open(chrome.runtime.getURL('options.html'));
+//});
